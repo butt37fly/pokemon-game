966282db51d10ec100f29dc7c327c6c9dbb0956e
Andrés <butt37fly@butt37fly.butt37fly-network>
2024-10-27 22:30:16 -0500

    Estilos corregidos

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  line-height: 1.1rem;
+  line-height: 100%;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
