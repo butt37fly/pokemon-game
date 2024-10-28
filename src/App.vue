75f54c87d2047dc84e169607a06ad59fc1ded345
Andrés <butt37fly@butt37fly.butt37fly-network>
2024-10-27 22:21:39 -0500

    Estilos generales de la aplicación definidos

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,29 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+:root {
+  --c-black: #1f1f1f;
+  --c-white: #fff;
+  --c-grey: #cccccc;
+  --c-purple-01: #0B0417;
+  --c-purple-02: #291746;
+
+  --c-background: var(--c-purple-01);
+  --c-text: var(--c-white);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  line-height: 1.1rem;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+body {
+  min-height: 100dvh;
+  color: var(--c-text);
+  background: radial-gradient(circle, var(--c-purple-02) 0%, var(--c-purple-01) 100%);
+}
+</style>
